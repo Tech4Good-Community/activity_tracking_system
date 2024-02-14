1dc38728a8200aba58e25547afd40746ef1ef880
ajith4Tech <ajit@tech4goodcommunity.com>
2024-02-14 16:55:11 +0530

    Added Funds Allocation Workflow (reflects in Database, will not reflect in backend)

--- activity_tracking_system/activity_tracking_system/doctype/funds_allocation/funds_allocation.js
+++ activity_tracking_system/activity_tracking_system/doctype/funds_allocation/funds_allocation.js
@@ -6,3 +6,25 @@
 
 // 	},
 // });
+
+frappe.ui.form.on('Funds Allocation', {
+    onload: function(frm) {
+        // Your onload code here
+    },
+    refresh: function(frm) {
+        if (frm.doc.__islocal) {
+            frm.remove_custom_button('Request Edit');
+        } else {
+            // Check if the user has permission to view the button
+            if (frappe.user.has_role('Human Resource Director')) {
+                frm.add_custom_button(__('Request Edit'), function() {
+                    // Custom button action
+                    // For example, show an alert
+                    frappe.msgprint('Custom button clicked!');
+                });
+            }
+        }
+    }
+});
+
+
