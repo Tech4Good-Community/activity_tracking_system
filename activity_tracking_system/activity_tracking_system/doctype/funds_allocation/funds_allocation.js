1ebed99aae8733de87165eb42d7a0b97e5a26de3
ajith4Tech <ajit@tech4goodcommunity.com>
2024-02-20 16:23:55 +0530

    Refactored the code base for funds allocation

--- activity_tracking_system/activity_tracking_system/doctype/funds_allocation/funds_allocation.js
+++ activity_tracking_system/activity_tracking_system/doctype/funds_allocation/funds_allocation.js
@@ -1,22 +1,32 @@
-// Copyright (c) 2024, Tech4Good Community and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Funds Allocation", {
-// 	refresh(frm) {
-
-// 	},
-// });
+//Requesting to calculate percentages of Direct cost and indirect cost.
 
 frappe.ui.form.on('Funds Allocation', {
     after_save: function(frm) {
         frappe.call({
             method: 'activity_tracking_system.activity_tracking_system.doctype.funds_allocation.funds_allocation.calculate_direct_cost',
             args: {
-                doc: frm.doc
+                doc: frm.doc,
+                salary_funds: frm.doc.salary_funds_allocated,
+                travel_funds: frm.doc.travel__funds_allocated,
+                activity_funds: frm.doc.activity_funds_allocated,
+                consultant_funds: frm.doc.consultant_funds_allocated,
+                equipment_funds: frm.doc.equipment_funds_allocated,
+                research_funds: frm.doc.research_funds_allocated,
+                indirect_cost : frm.doc.indirect_funds_allocated
+                
             },
             callback: function(r) {
                 if(r.message) {
-                    console.log(r.message[0])
+                    frappe.model.set_value(frm.doctype, frm.docname, 'salary_percentage_of_allocation', r.message["salary_percentage"]);
+                    frappe.model.set_value(frm.doctype, frm.docname, 'travel_percentage_of_allocation', r.message["travel_percentage"]);
+                    frappe.model.set_value(frm.doctype, frm.docname, 'activity__percentage_of_allocation',r.message["activity_percentage"]);
+                    frappe.model.set_value(frm.doctype, frm.docname, 'consultant_percentage_of_allocation',r.message["consultant_percentage"]);
+                    frappe.model.set_value(frm.doctype, frm.docname, 'equipment_percentage_of_allocation',r.message["equipment_percentage"]);
+                    frappe.model.set_value(frm.doctype, frm.docname, 'research_percentage_of_allocation', r.message["research_percentage"]);
+                    frappe.model.set_value(frm.doctype, frm.docname, 'indirect_percentage_of_allocation', r.message["indirect_cost_percentage"]);
+                    frappe.model.set_value(frm.doctype, frm.docname, 'grand_total', r.message["grand_total"])
+
+                    doc.save()
                 }
             }
         })
--- activity_tracking_system/activity_tracking_system/doctype/funds_allocation/funds_allocation.py
+++ activity_tracking_system/activity_tracking_system/doctype/funds_allocation/funds_allocation.py
@@ -6,28 +6,35 @@ class FundsAllocation(Document):
         if self.funds_for_the_project == 0:
             frappe.throw(title='Error', msg='Please fill funds for the project field')
         
-        if not self.get('__islocal'):  # Check if the document is not being newly created
-            calculate_direct_cost(self)
 
 @frappe.whitelist()
-def calculate_direct_cost(doc):
-    salary = doc.salary_funds_allocated
-    funds_for_the_project = doc.funds_for_the_project
-    travel_funds_allocated = doc.travel__funds_allocated  
-    activity_funds_allocated = doc.activity_funds_allocated
-    consultant_funds_allocated = doc.consultant_funds_allocated
-    equipment_funds_allocated = doc.equipment_funds_allocated
-    research_funds_allocated = doc.research_funds_allocated
+def calculate_direct_cost(doc, salary_funds, travel_funds, equipment_funds, research_funds, activity_funds, consultant_funds, indirect_cost):
+    salary = float(salary_funds)
+    #funds_for_the_project = float(funds_for_project)
+    #print(funds_for_the_project)
+    travel_funds_allocated = float(travel_funds)
+    activity_funds_allocated = float(activity_funds)
+    consultant_funds_allocated = float(consultant_funds)
+    equipment_funds_allocated = float(equipment_funds)
+    research_funds_allocated = float(research_funds)
+    indirect_cost = float(indirect_cost)
     
     total_direct_cost = sum([salary, travel_funds_allocated, activity_funds_allocated, consultant_funds_allocated, equipment_funds_allocated, research_funds_allocated])
-    print(total_direct_cost)
+    
+    grand_total = total_direct_cost + indirect_cost
+    
+    if grand_total != 0:
+        indirect_cost_percentage = round(((indirect_cost/ grand_total) * 100), 1)
+    else:
+        indirect_cost_percentage = 0
+    
     if total_direct_cost != 0:
-        salary_percentage = round(((salary / total_direct_cost) * 100),2)
-        travel_percentage = round(((travel_funds_allocated / total_direct_cost) * 100),2)
-        activity_percentage = round(((activity_funds_allocated / total_direct_cost) * 100),2)
-        consultant_percentage =  round(((consultant_funds_allocated / total_direct_cost) * 100),2)
-        equipment_percentage = round(((equipment_funds_allocated / total_direct_cost) * 100),2)
-        research_percentage = round(((research_funds_allocated / total_direct_cost) * 100),2)
+        salary_percentage = round(((salary / total_direct_cost) * 100), 1)
+        travel_percentage = round(((travel_funds_allocated / total_direct_cost) * 100), 2)
+        activity_percentage = round(((activity_funds_allocated / total_direct_cost) * 100), 2)
+        consultant_percentage = round(((consultant_funds_allocated / total_direct_cost) * 100), 2)
+        equipment_percentage = round(((equipment_funds_allocated / total_direct_cost) * 100), 2)
+        research_percentage = round(((research_funds_allocated / total_direct_cost) * 100), 2)
     else:
         salary_percentage = 0
         travel_percentage = 0
@@ -35,8 +42,18 @@ def calculate_direct_cost(doc):
         consultant_percentage = 0
         equipment_percentage = 0
         research_percentage = 0
-    return_list = [salary_percentage, travel_percentage, activity_percentage, consultant_percentage, equipment_percentage, research_percentage] 
-    print(return_list) 
-    return return_list
-        #frappe.set_value(doc.doctype, doc.name, 'salary_percentage_of_allocation', salary_percentage)
+    data = {
+        "salary_percentage" : salary_percentage,
+        "travel_percentage" : travel_percentage,
+        "activity_percentage": activity_percentage,
+        "consultant_percentage":consultant_percentage,
+        "equipment_percentage": equipment_percentage,
+        "research_percentage" : research_percentage,
+        "indirect_cost_percentage": indirect_cost_percentage,
+        "grand_total" : grand_total
+        
+    }
+    
+    return data
+
 
