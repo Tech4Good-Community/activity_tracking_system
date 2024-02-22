4261f6db7f80d6bd2b6c4917cac31bec923a4944
CHANDRU M <chandru@tech4goodcommunity.com>
2024-02-22 12:44:12 +0530

    Feat: Data validation for Activity doctype: Activity Initiation Date and Activity Completion Date. Added City Manger Report, State Manager Report, Director Program Report, Procurement Department Report, HR and Admin Department Report, Finance Department Report

--- activity_tracking_system/activity_tracking_system/doctype/activity/activity.py
+++ activity_tracking_system/activity_tracking_system/doctype/activity/activity.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2024, Tech4Good Community and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Activity(Document):
-	pass
+	def validate(self):
+		if self.activity_completion_date < self.activity_process_initiating_date:
+				frappe.throw(title='Error', msg='Initiation date cannot be greater than completion date')
+
