cead67975996c4c0ad27a9d3c807a8d9ba8a893a
ajith4Tech <ajit@tech4goodcommunity.com>
2024-02-26 12:47:54 +0530

    Fix: Str type of activity to be completed in which month

--- activity_tracking_system/activity_tracking_system/doctype/activity/activity.py
+++ activity_tracking_system/activity_tracking_system/doctype/activity/activity.py
@@ -6,9 +6,11 @@ from frappe.model.document import Document
 
 
 class Activity(Document):
-	def validate(self):
-		if self.activity_completion_date:
-			if self.activity_completion_date < self.activity_process_initiating_date:
-					frappe.throw(title='Error', msg='Initiation date cannot be greater than completion date')
-		if self.activity_to_be_completed_in_which_month < self.activity_process_initiating_date:
-					frappe.throw(title='Error', msg='Completion date cannot be lesser than intitalisation date')
+    def validate(self):
+        if self.activity_completion_date:
+            if self.activity_completion_date < self.activity_process_initiating_date:
+                frappe.throw(title='Error', msg='Initiation date cannot be greater than completion date')
+        if self.activity_to_be_completed_in_which_month:
+            if self.activity_to_be_completed_in_which_month < self.activity_process_initiating_date:
+                frappe.throw(title='Error', msg='Completion date cannot be lesser than initiation date')
+
