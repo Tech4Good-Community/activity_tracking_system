65f2ab9f03a69ff8aec66834d1ef44949460a10e
ajith4Tech <ajit@tech4goodcommunity.com>
2024-03-11 13:54:47 +0530

    Fix: Project End date error is fixed

--- activity_tracking_system/activity_tracking_system/doctype/project/project.py
+++ activity_tracking_system/activity_tracking_system/doctype/project/project.py
@@ -7,6 +7,6 @@ from frappe.model.document import Document
 
 class Project(Document):
     def validate(self):
-        if self.project_end_date:
-            if self.project_end_date <= self.project_start_date:
+        if self.end_date:
+            if self.end_date <= self.start_date:
                 frappe.throw(title='Error', msg='Project End date cannot be lesser than Project Start date')
