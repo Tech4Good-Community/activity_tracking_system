2db16e8149024181e258327e9bddd16f294147f6
neuron <praveen.902012@gmail.com>
2024-06-28 15:53:19 +0530

    Added Department as filter to activity page and added css

--- activity_tracking_system/activity_tracking_system/doctype/activity/activity.py
+++ activity_tracking_system/activity_tracking_system/doctype/activity/activity.py
@@ -39,7 +39,6 @@ def create_activity_in_planning_sheet(doc_name):
         frappe.msgprint(f"Activity {doc_name} already exists in the planning sheet.")
 
 
-
 @frappe.whitelist()
 def get_all_activities(filters=None):
     try:
@@ -48,25 +47,37 @@ def get_all_activities(filters=None):
         start_of_month = current_date.replace(day=1)
         end_of_month = current_date.replace(day=1, month=current_date.month % 12 + 1, year=current_date.year if current_date.month < 12 else current_date.year + 1) - datetime.timedelta(days=1)
 
+        activity_filters = {
+            'activity_to_be_completed_in_which_month': ['>=', start_of_month],
+            'activity_to_be_completed_in_which_month': ['<=', end_of_month],
+        }
+            
+        if 'supporting_department' in filters and filters['supporting_department']:
+            activity_filters['assigned_to_which_department_for_support'] = filters['supporting_department']
 
+        print(activity_filters)
         activities = frappe.db.get_list('Activity',
             fields=['name', 'activity', 'type_of_activity', 'city', 'assigned_to_which_department_for_support', 'activity_to_be_completed_in_which_month'],
+            filters=activity_filters
+        )
+
+        notification_activities = frappe.db.get_list('Activity',
+            fields=['activity', 'activity_completion_date'],
             filters={
-                'activity_to_be_completed_in_which_month': ['>=', start_of_month],
-                'activity_to_be_completed_in_which_month': ['<=', end_of_month],
-            })
+                'activity_completion_date': ['>=', current_date],
+                'activity_completion_date': ['<=', end_of_month]
+            }
+        )
 
-        notification_activities = frappe.db.get_list('Activity', fields=['activity', 'activity_completion_date'],filters={
-        'activity_completion_date': ['>=', current_date],
-        'activity_completion_date': ['<=', end_of_month]
-    })
-
+        # Filter out activities without a completion date
         notification_activities = [activity for activity in notification_activities if activity.get('activity_completion_date') is not None]
 
+        # Format dates for display
         for activity in activities:
             activity['activity_to_be_completed_in_which_month'] = frappe.utils.formatdate(activity['activity_to_be_completed_in_which_month'], 'MMMM YYYY')
         for activity in notification_activities:
             activity['activity_completion_date'] = frappe.utils.formatdate(activity['activity_completion_date'], 'dd MMMM YYYY')
+        
         return activities, notification_activities
     except Exception as e:
         frappe.log_error(message=frappe.get_traceback(), title="Error in get_activities_for_current_month")
